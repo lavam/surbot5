c118a78fc22d332ad252e5c26a4cf24c03c668e7
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-02-25 13:27:00 +0530

    Update function.js

--- function.js
+++ function.js
@@ -6,9 +6,9 @@ function promptFn() {
 prompt ("Prompt box", "Welcome");
 }
 
-function getSysDateTime() {
-var systoday = new Date();
-var sysdate =  today.getDate() + "." + (today.getMonth()+1) + "." + today.getFullYear()+;
-var systime= today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var sysDateTime = sysdate +", "+ systime;
+function getDateTime() {
+var today = new Date();
+var date =  today.getDate() + "." + (today.getMonth()+1) + "." + today.getFullYear()+;
+var time= today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    dateTime = date +", "+ time;
 }
