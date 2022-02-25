862f1f8b7768bd47ee5b34ec1b6cb692f5b29752
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-02-25 13:54:57 +0530

    Update function.js

--- function.js
+++ function.js
@@ -8,7 +8,7 @@ prompt ("Prompt box", "Welcome");
 
 function getDateTime() {
 var today = new Date();
-var date =  today.getDate() + "." + (today.getMonth()+1) + "." + today.getFullYear()+;
+var date =  today.getDate() + "." + (today.getMonth()+1) + "." + today.getFullYear();
 var time= today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     dateTime = date +", "+ time;
 }
